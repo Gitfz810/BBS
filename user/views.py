d96a79f5fc6e80a0eaa6b604ced373388e7723ac
Gitfz810 <mailfz810@163.com>
2018-08-27 15:37:41 +0800

    完成user模块

--- user/views.py
+++ user/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
 
 from user.forms import RegisterForm
+from user.models import User
+from user.helper import login_required
 
 
 def register(request):
@@ -13,6 +15,11 @@ def register(request):
             # 密码加密保存
             user.password = make_password(user.password)
             user.save()
+
+            # 设置登录状态
+            request.session['uid'] = user.id
+            request.session['nickname'] = user.nickname
+
             return redirect('/user/info/')
         else:
             return render(request, 'register.html', {'error': form.errors})
@@ -20,14 +27,30 @@ def register(request):
 
 
 def login(request):
-    return render(request, 'login.html', {})
+    if request.method == 'POST':
+        nickname = request.POST.get('nickname')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(nickname=nickname)
+        except User.DoesNotExist:
+            return render(request, 'login.html', {'error': '用户不存在'})
+        else:
+            if check_password(password, user.password):
+                # 设置登录状态
+                request.session['uid'] = user.id
+                request.session['nickname'] = user.nickname
+                return redirect('/user/info/')
+            else:
+                return render(request, 'login.html', {'error': '用户名或密码错误'})
+    return render(request, 'login.html')
 
 
 def logout(request):
-    return render(request, 'logout.html', {})
+    request.session.flush()
+    return redirect('/user/login/')
 
-
+@login_required
 def user_info(request):
-    return render(request, 'user_info.html', {})
-
-
+    uid = request.session.get('uid')
+    user = User.objects.get(id=uid)
+    return render(request, 'user_info.html', {'user': user})
